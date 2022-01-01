ec074c56dd9dfbfe04f9ea57b89b27517ada7b15
HIDEJIRO <hidejiro@live.jp>
2022-01-01 19:39:13 +0900

    初回コミット

---
+++ WpfApp1/WpfApp1/Tables/TantoMaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Linq.Mapping;
+
+namespace WpfApp1.Tables
+{
+    [Table(Name = "tanto")]
+    public class TantoMaster
+    {
+        [Column(Name = "no", IsPrimaryKey = true)]
+        public int No { get; set; }
+        [Column(Name = "name")]
+        public String Name { get; set; }
+        [Column(Name = "cdate")]
+        public String cDate { get; set; }
+        [Column(Name = "edate")]
+        public String eDate { get; set; }
+    }
+}
